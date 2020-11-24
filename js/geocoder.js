21a588e33c10994cc7cf8c454146145bae5443d1
kuberaspeaking <kuber.shruti@gmail.com>
2020-11-24 11:39:36 +0100

    added marker

--- js/geocoder.js
+++ js/geocoder.js
@@ -1,13 +1,21 @@
 var geocoder = platform.getSearchService();
-
+var peopleGroup = new H.map.Group();
+map.addObject(peopleGroup);
 function showPeople(){
-    fetch('peopleData.json')
+    fetch('peopleData.json') // call to your DB
     .then(response => response.json())
     .then(data =>{
         data.people.forEach(async person=>{
             let position = await showPosition(person.address);//get postion for person
             // place a marker
-            console.log(position);
+
+            let peopleMarker = new H.map.Marker(position);
+              // add marker to map
+            peopleGroup.addObject(peopleMarker);
+            // console.log(position);
+            map.getViewModel().setLookAtData({
+                bounds: peopleGroup.getBoundingBox()
+            });
         })
         console.log(data)})
 }
